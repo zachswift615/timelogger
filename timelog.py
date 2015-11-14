f43a983f5eb089a335c577837f45515b23b9542a
Zach Swift <zachary.swift@healthways.com>
2015-11-14 15:00:47 -0600

    create, read, delete working

--- timelog.py
+++ timelog.py
@@ -3,59 +3,157 @@ import sqlite3, datetime, textwrap
 # conn = sqlite3.Connection('timelog')
 # curs = conn.cursor()
 #
-# TABLEDEF = 'create table timelog (timestamp text, description text);'
+# drop = "drop table if exists timelog;"
+# curs.execute(drop)
+# conn.commit()
+# conn.close()
+#
+#
+# conn = sqlite3.Connection('timelog')
+# curs = conn.cursor()
+# TABLEDEF = '''
+#     create table timelog
+#     (
+#     id integer primary key autoincrement,
+#     timestamp text,
+#     description text
+#     );
+#     '''
 # curs.execute(TABLEDEF)
+# conn.commit()
+# conn.close()
 
 def new_entry(desc, ts=None):
     if not ts:
         ts = datetime.datetime.now().isoformat()
-    query = 'insert into timelog values ("{}", "{}");'.format(ts, desc)
+    query = 'insert into timelog values (null, "{}", "{}");'.format(ts, desc)
     try:
         conn = sqlite3.Connection('timelog')
         curs = conn.cursor()
-        curs.execute(query)
+        res = curs.execute(query)
         conn.commit()
         conn.close()
-        return True
+        return res
     except Exception as e:
         return e
 
-def get_entries(range=None):
+def delete_entry(entry_id):
     conn = sqlite3.Connection('timelog')
     curs = conn.cursor()
-    query = 'SELECT * FROM timelog where date(timestamp)=date("now", "-1 day") ORDER BY datetime(timestamp) DESC;'
+    query = 'DELETE FROM timelog where id={};'.format(entry_id)
     myres = curs.execute(query)
     myres = myres.fetchall()
     conn.commit()
     conn.close()
     return myres
 
+def read_entry(entry_id):
+    conn = sqlite3.Connection('timelog')
+    curs = conn.cursor()
+    query = 'select * FROM timelog where id={};'.format(entry_id)
+    myres = curs.execute(query)
+    myres = myres.fetchall()
+    conn.commit()
+    conn.close()
+    return myres
+
+def get_entries_by_day(date_string=datetime.date.today().isoformat()):
+    conn = sqlite3.Connection('timelog')
+    curs = conn.cursor()
+    query = '''
+        SELECT * FROM timelog
+        where date(timestamp)=date("{}")
+        ORDER BY datetime(timestamp) ASC;
+        '''.format(date_string)
+    myres = curs.execute(query)
+    myres = myres.fetchall()
+    conn.commit()
+    conn.close()
+    return myres
+
+def edit_entry(timestamp):
+    conn = sqlite3.Connection('timelog')
+    curs = conn.cursor()
+    query = 'SELECT * FROM timelog where timestamp = "{}";'.format(timestamp)
+    myres = curs.execute(query)
+    ts, desc = myres.fetchone()
+    conn.commit()
+    conn.close()
+
 if __name__ == '__main__':
+    time_format = '%I:%M:%S %p'
+    datetime_format = '%b %d, %Y %I:%M %p'
+    parse_format = '%Y-%m-%dT%H:%M:%S.%f'
     while True:
-        choice = input('new entry: [n], list entries: [l], exit: [x]: ')
-        if choice.lower() == 'n':
+        print('\n' + '*' * 79 + '\n')
+        choice_format = '{0:<15}{1:<15}{2:<15}{3:<15}'
+        prompt = choice_format.format('create', 'read', 'delete', 'exit')
+        prompt += '\n' + choice_format.format('[c]', '[r]', '[d]', '[x]')
+        prompt += '\n' + '>>'
+        choice = input(prompt)
+        print('\n' + '*' * 79 + '\n')
+        if choice.lower() == 'c':
             desc = input('enter description: ')
             ts = input('enter time (leave blank for now): ')
             if not ts:
                 resp = new_entry(desc)
             else:
                 resp = new_entry(desc, ts)
+            print(resp)
             if resp:
                 print('entry successfully added')
             else:
                 print(resp)
-        elif choice.lower() == 'l':
-            res = get_entries()
-            for entry in res:
-                ts, desc = entry
-                desc_lines = textwrap.wrap(desc)
-                print('='*79)
-                print(ts)
-                print('-'*79)
-                print('\n')
-                for line in desc_lines:
-                    print(line)
-                print('\n')
+        elif choice.lower() == 'r':
+            user_choice = input("by [day], or by [id]?\n>> ")
+            if user_choice == 'id':
+                entry_id = input('entry ID: ')
+                res = read_entry(entry_id)
+                print(res)
+                for entry in res:
+                    id, ts, desc = entry
+                    desc_lines = textwrap.wrap(desc)
+                    print('='*79)
+                    myformat = '{0:<40}{1:>39}'
+                    idinfo = 'entry ID: {}'.format(id)
+                    dt = datetime.datetime.strptime(ts, parse_format)
+                    pretty_date = dt.strftime(datetime_format)
+                    print(myformat.format(pretty_date, idinfo))
+                    print('-'*79)
+                    print('\n')
+                    for line in desc_lines:
+                        print(line)
+                    print('\n')
+
+            elif user_choice == 'day':
+                day_string = input('select day (leave blank for today): ')
+
+                if day_string:
+                    res = get_entries_by_day(day_string)
+                else:
+                    res = get_entries_by_day()
+
+                for entry in res:
+                    id, ts, desc = entry
+                    desc_lines = textwrap.wrap(desc)
+                    print('='*79)
+                    myformat = '{0:<40}{1:>39}'
+                    idinfo = 'entry ID: {}'.format(id)
+                    dt = datetime.datetime.strptime(ts, parse_format)
+                    pretty_date = dt.strftime(datetime_format)
+                    print(myformat.format(pretty_date, idinfo))
+                    print('-'*79)
+                    print('\n')
+                    for line in desc_lines:
+                        print(line)
+                    print('\n')
+
+        elif choice.lower() == 'd':
+            entry_id = input('entry ID: ')
+            res = delete_entry(entry_id)
+            print(res)
+
+
         elif choice.lower() == 'x':
             exit()
 
